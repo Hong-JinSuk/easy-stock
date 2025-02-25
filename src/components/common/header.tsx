a4aec091ba1067d264743c72d58e8e68bb9f033d
HongJinSuk <realtone98@penta.co.kr>
2025-02-25 18:05:46 +0900

    minor

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,29 +3,33 @@ import { languageData } from '@/store/language';
 import { sidebarMenus } from '@/store/sidebar';
 import { useAtom } from 'jotai';
 import { Bell, Menu, Search } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SelectLanguage } from '../ui/select-language';
 import AvatarDropdown from './avatar-dropdown';
+
+const langData = languageData;
+const Menus = sidebarMenus;
 
 export default function Header() {
   const [sidebar, setSidebar] = useAtom(sidebarAtom);
   const [language, setLanguage] = useAtom(languageATom);
   const [searchValue, setSearchValue] = useState<string>('');
-  const langData = languageData;
-
-  useEffect(() => {
-    console.log(language);
-  }, [language]);
+  const [selectedMenus, setSelectedMenus] = useState<string>('News');
 
   const onClickSidebarMenu = () => {
     setSidebar((prev) => !prev);
   };
 
-  const Menus = sidebarMenus;
+  const onClickMenu = (menu: React.MouseEvent<HTMLSpanElement>) => {
+    const target = menu.currentTarget as HTMLSpanElement;
+    setSelectedMenus(target.innerText);
+  };
 
   return (
-    <header className="w-full flex flex-col items-center justify-center 2xl:mx-auto md:min-h-[75px] lg:max-h-[135px] sticky top-0 bg-background border-b-2 border-blue-900">
-      <div className="w-full max-w-screen-2xl flex items-center justify-between px-4 gap-4 min-h-[65px] max-h-[75px]">
+    <header
+      className={`w-full flex flex-col items-center justify-center 2xl:mx-auto md:min-h-[75px] lg:max-h-[135px]  sticky top-0 bg-background border-b-2 border-blue-900`}
+    >
+      <div className="w-full max-w-screen-2xl flex items-center justify-between px-4 gap-4 min-h-[75px] max-h-[75px]">
         <div className="flex items-center justify-center gap-2">
           <Menu
             className="cursor-pointer hover:bg-accent hover:bg-green-100 rounded-lg p-1.5 size-8"
@@ -48,7 +52,12 @@ export default function Header() {
             className="hidden items-center justify-center lg:flex"
             key={index}
           >
-            {`${menu[`menuNm_${language}` as keyof typeof menu]}`}
+            <span
+              className={`hover:text-green-700 cursor-pointer font-bold text-sm ${
+                selectedMenus === menu['menuNm_ENG'] && 'text-green-700'
+              }`}
+              onClick={(e) => onClickMenu(e)}
+            >{`${menu[`menuNm_${language}` as keyof typeof menu]}`}</span>
           </div>
         ))}
         <div className="flex items-center justify-center gap-2">
@@ -63,7 +72,17 @@ export default function Header() {
           <AvatarDropdown />
         </div>
       </div>
-      <div className="w-full md:h-[70px] bg-slate-200">dsiofjoi</div>
+      <div
+        className={`mobile-hidden w-full items-center justify-center bg-slate-200 2xl:mx-auto  ${
+          selectedMenus === 'Finance'
+            ? 'md:min-h-[50px] max-h-[50px]'
+            : 'h-0 opacity-0 overflow-hidden'
+        }`}
+      >
+        <div className="w-full max-w-screen-2xl flex items-center justify-between px-4 gap-4">
+          dfijosdfdjiso
+        </div>
+      </div>
     </header>
   );
 }
